917f011cde46f0fbb1befdf93549b87d0dfddb4f
Shank Spencer <shank.jay.spencer@gmail.com>
2022-03-23 21:08:22 +0800

    Added smartbankreact dir

---
+++ smartbankreact/src/Constants.js
@@ -0,0 +1,3 @@
+export const API_URL = "http://localhost:8081/";
+//export const API_URL = "http://lab2202u18-app.brainupgrade.in/api/";
+// export const API_URL = "https://reksu18-app1.brainupgrade.net/";
